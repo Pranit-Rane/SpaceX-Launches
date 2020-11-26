14cc526e704d607b601d13868c034bf87332de2c
Pranit Rane <pranitrane988@gmail.com>
2020-11-26 06:30:04 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Filter from './Components/Filter';
+import List from './Components/List';
 
-function App() {
+class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  state = {
+    launchedStatus: undefined,
+    landStatus: undefined,
+    launchYear: undefined
+  };
+
+  filterSuccessMainLaunch = (value) => {
+    this.setState(() => ({ launchedStatus: value }));
+  }
+
+  filterSuccessMainLand = (value) => {
+    this.setState(() => ({ landStatus: value }));
+  }
+
+  filterSuccessMainYear = (value) => {
+    this.setState(() => ({ launchYear: value }));
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <h1>SpaceX Launch Programs</h1>
+        <div className="row top-buffer">
+          <div className="col-md-3 col-xs-3" align="left">
+            <Filter filterSuccessMainLaunch={this.filterSuccessMainLaunch} filterSuccessMainLand={this.filterSuccessMainLand} filterSuccessMainYear={this.filterSuccessMainYear}/>
+          </div>
+          <div className="col-md-9 col-xs-9" align="left">
+            <List launchedStatus={this.state.launchedStatus} landStatus={this.state.landStatus} launchYear={this.state.launchYear}/>
+          </div>  
+        </div>
+      </div>
     </div>
   );
 }
+}
+
 
 export default App;
